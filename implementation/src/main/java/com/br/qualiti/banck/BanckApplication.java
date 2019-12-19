80b775f17fe7bf80efdde344d83805ce86a7b2cf
Rafael_Duarte <rafaelbatistaduarte@gmail.com>
2019-12-18 22:57:16 -0300

    Aplication startup.

---
+++ implementation/src/main/java/com/br/qualiti/banck/BanckApplication.java
@@ -0,0 +1,13 @@
+package com.br.qualiti.banck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanckApplication.class, args);
+	}
+
+}
---
+++ implementation/src/test/java/com/br/qualiti/banck/BanckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.qualiti.banck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
