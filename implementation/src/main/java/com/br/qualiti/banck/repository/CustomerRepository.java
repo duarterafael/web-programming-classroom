16e07f07461c8a53addd7dba3d14330c6a00f057
Rafael_Duarte <rafaelbatistaduarte@gmail.com>
2020-01-02 09:27:17 -0300

    Add comment in Repository layer

--- implementation/src/main/java/com/br/qualiti/banck/repository/CustomerRepository.java
+++ implementation/src/main/java/com/br/qualiti/banck/repository/CustomerRepository.java
@@ -5,6 +5,13 @@ import org.springframework.stereotype.Repository;
 
 import com.br.qualiti.banck.model.Customer;
 
+/*
+ * Com a classe modelo criada, o próximo passo é criar o repositório (ou DAO: Data Access Object) 
+ * que irá fornecer os métodos para as operações CRUD. 
+ * Uma forma fácil de fazer isso é criar uma interface que extende a interface JpaRepository (do Spring Data):
+ * A interface JpaRepository precisa de dois parâmetros do tipo Generics: o primeiro é a entidade JPA que representa a tabela 
+ * e o segundo é o tipo da chave primária (o mesmo tipo do atributo id).
+ */
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
