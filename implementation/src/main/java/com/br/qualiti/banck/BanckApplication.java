c7c1f9522f4ce9913fc317a917bc220f1cec927b
rbd <rbd@cin.ufpe.br>
2020-01-17 13:24:32 -0300

    Cros configuration

--- implementation/src/main/java/com/br/qualiti/banck/BanckApplication.java
+++ implementation/src/main/java/com/br/qualiti/banck/BanckApplication.java
@@ -3,12 +3,25 @@ package com.br.qualiti.banck;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //Para entender melhor as APIs criadas acesse http://localhost:8080/swagger-ui.html
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BanckApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BanckApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				 registry.addMapping("/**");
+			}
+		};
 	}
 
 }
