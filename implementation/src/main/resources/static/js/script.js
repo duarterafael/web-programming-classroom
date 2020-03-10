f3b78f9c8a071f89fdab785e27cbc86e916a2e2c
RicardoMVA <ricardo-valenca@hotmail.com>
2020-03-10 13:19:07 -0300

    add logic and style to display results in a ul

--- implementation/src/main/resources/static/js/script.js
+++ implementation/src/main/resources/static/js/script.js
@@ -1,5 +1,7 @@
 const display = document.getElementById("p-tag");
 
+const displayList = document.getElementById("ul-tag");
+
 $.ajax({
   type: 'GET',
   url: 'http://localhost:8080/api/v1/customers',
@@ -7,7 +9,13 @@ $.ajax({
     // displays the response as a string
     display.innerHTML = JSON.stringify(result);
 
-    
+    // display each entry in a list
+    result.forEach((entry) => {
+      const newLine = document.createElement("li");
+      newLine.innerHTML = JSON.stringify(entry);
+
+      displayList.appendChild(newLine);
+    })
   },
   error: function(result) {
     display.innerHTML = JSON.stringify(result);
