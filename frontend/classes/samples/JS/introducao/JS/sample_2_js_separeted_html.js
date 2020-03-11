02a1e8f4686243429dbf06b63473a9339f4d9936
Rafael Batista Duarte <rafaelbatistaduarte@gmail.com>
2020-03-11 15:58:35 -0300

    Aula JS - Introducao

---
+++ frontend/classes/samples/JS/introducao/JS/sample_2_js_separeted_html.js
@@ -0,0 +1,3 @@
+window.onload = function() {
+    alert("Olá pessoal lindo do meu RECODE!");
+}
---
+++ frontend/classes/samples/JS/introducao/JS/sample_3_js_separeted_html.js
@@ -0,0 +1,11 @@
+var numero = 0;
+var numeroComCasasDecimais = 1.405;
+var texto = 'Variável com um texto';
+var outroTexto = " Outra variável com um texto ";
+var valorBooleano = true;
+
+console.log(numero);
+console.log(numeroComCasasDecimais);
+console.log(texto);
+console.log(outroTexto);
+console.log(valorBooleano);
