ef0ee821d5f4894376199ed1a1c93b0ddd33a392
Rafael <rafaelbatistaduarte@gmail.com>
2020-07-20 11:21:47 -0300

    docker integration

---
+++ implementation/build.bat
@@ -0,0 +1,7 @@
+cd %CD%
+docker-compose -f "docker-compose.yml" down
+
+docker-compose -f "docker-compose.yml" up -d --build mysqldb
+
+docker-compose -f "docker-compose.yml" up -d --build adminer
+
